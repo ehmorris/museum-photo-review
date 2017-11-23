ff9fe6c511ce2834f072ad0509c77de6c829b7f5
Edwin Morris <edwin@rapgenius.com>
2017-11-23 14:25:23 -0500

    Add helper to determine if coordinates are near museum

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,28 @@
 module ApplicationHelper
+  def coordinates_are_near_museum(latitude, longitude)
+    RestClient.get('https://en.wikipedia.org/w/api.php', {
+      params: {
+        action: 'query',
+        prop: 'categories',
+        generator: 'geosearch',
+        cllimit: 500,
+        ggscoord: "#{latitude}|#{longitude}",
+        ggsradius: 200,
+        ggslimit: 10,
+        format: 'json',
+        origin: '*'
+      },
+      headers: {
+        'Api-User-Agent': 'MuseumPhotoReview/0.0.1'
+      }
+    }) { |response|
+      locations = JSON.parse(response)
+
+      if locations['query'] && locations['query']['pages']
+        pages = locations['query']['pages']
+      end
+
+      pages ? pages.to_s.downcase().include?('museum') : false
+    }
+  end
 end
