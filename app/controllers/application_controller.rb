df63d890bd93c6d8e05d5a011cb9c975af391454
Edwin Morris <edwin@rapgenius.com>
2017-11-23 11:53:31 -0500

    Setup Dropbox gem

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def show
+    client = DropboxApi::Client.new(ENV['DROPBOX_ACCESS_TOKEN'])
+
+    @result = client.list_folder('/camera uploads', {
+      include_media_info: true
+    })
+  end
 end
